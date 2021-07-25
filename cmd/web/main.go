f6a2ad840b9ba577ca6a8754e15a50e5ae8cc301
alonzzio <57526912+alonzzio@users.noreply.github.com>
2021-07-25 21:25:13 +0930

    Implemented simple web server with Home page from template html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+const portNumber = ":8080"
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting web server on " + portNumber)
+	_ = http.ListenAndServe(portNumber, mux)
 }
---
+++ cmd/web/route.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/alonzzio/go-chat/internal/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	return mux
+}
